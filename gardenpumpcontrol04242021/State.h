dc5c93d40ab90b4a3dcbc81153e6ec72dffbbb48
brian <bcburnett@gmail.com>
2021-04-24 17:15:32 -0400

    .

--- gardenpumpcontrol04242021/State.h
+++ gardenpumpcontrol04242021/State.h
@@ -1,22 +1,22 @@
 /*
-* Garden sensor sketch
-* File: State.h
-* Author: Brian C. Burnett
-* GitHub repo: https://github.com/bcburnett/garden
-* Directory: https://github.com/bcburnett/garden/tree/master/garden
-* Questions: https://github.com/bcburnett/garden/discussions/1
-* 
-* The relay for the water solenoid is connected to pin 2 of the esp32 
-*   to give a visual indicator .
-*   
-* The capacitive moisture sensor analog output is connected to pin 32.
-* 
-* This application is in the Public Domain.
-* All information is provided in good faith, however I make no representation 
-* or warranty of any kind, express or implied, regarding the accuracy, adequacy, 
-* validity, reliability, availability or completeness of this application.
- */
- 
+  Garden sensor sketch
+  File: State.h
+  Author: Brian C. Burnett
+  GitHub repo: https://github.com/bcburnett/garden
+  Directory: https://github.com/bcburnett/garden/tree/master/garden
+  Questions: https://github.com/bcburnett/garden/discussions/1
+
+  The relay for the water solenoid is connected to pin 2 of the esp32
+    to give a visual indicator .
+
+  The capacitive moisture sensor analog output is connected to pin 32.
+
+  This application is in the Public Domain.
+  All information is provided in good faith, however I make no representation
+  or warranty of any kind, express or implied, regarding the accuracy, adequacy,
+  validity, reliability, availability or completeness of this application.
+*/
+
 #pragma once
 #include<Arduino.h>
 #include<map>
@@ -25,20 +25,20 @@
 class State {
   public:
 
-float humidity();
-float humidity(float value);
+    float humidity();
+    float humidity(float value);
 
-float pressure();
-float pressure(float value);
+    float pressure();
+    float pressure(float value);
 
-double pressure64();
-double pressure64(double value);
+    double pressure64();
+    double pressure64(double value);
 
-float temperature();
-float temperature(float value);
+    float temperature();
+    float temperature(float value);
 
     // setter and getter for the threshold TODO
-    int threshold();  
+    int threshold();
     int threshold(int value);
 
     // setter and getter for the uploading filename
